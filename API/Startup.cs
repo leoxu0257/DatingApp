429fed502204bfae371e446ee17eddbe8125f58c
leoxu0257 <enmacorradi@163.com>
2021-03-24 11:21:52 +1300

    add config

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,12 +18,13 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
+        // public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
