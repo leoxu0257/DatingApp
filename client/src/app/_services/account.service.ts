fb8d7f4c9ac1ed639869b5c06ddd2566ebb44756
leoxu0257 <enmacorradi@163.com>
2021-04-01 14:47:52 +1300

    add home page

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -9,7 +9,7 @@ import { User } from '../_models/user';
 })
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
-  private currentUserSource = new ReplaySubject<User>(1);
+  public currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,19 +11,19 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
+  // loggedIn: boolean;
+  // currentUser$: Observable<User>;
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    // this.currentUser$ = this.accountService.currentUser$;
   }
   login() {
-    // console.log(this.model);
+    // console.log(this.model); 
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
-
+      // this.loggedIn = true;
     }, error => {
       console.log(error);
     })
@@ -29,16 +31,16 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
+    // this.loggedIn = false;
   }
 
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user;
-    }, error=>{
-      console.log(error);
+  // getCurrentUser() {
+  //   this.accountService.currentUser$.subscribe(user => {
+  //     this.loggedIn = !!user;
+  //   }, error=>{
+  //     console.log(error);
       
-    });
-  }
+  //   });
+  // }
 
 }
